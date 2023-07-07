f573314ed12b12a09d4002edad1756259e6b8d7b
pande aditya <pande aditya@LAPTOP-TLJ9M0NB>
2023-07-08 01:30:25 +0800

    Update

---
+++ src/uas_sembako/Penjualan.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uas_sembako;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pande aditya
+ */
+public class Penjualan {
+     private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException{
+             try {
+        String url = "jdbc:mysql://localhost:3306/datamaster";
+        String user = "root";
+        String password = "";
+        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        mysqlconfig = DriverManager.getConnection(url, user, password);
+        }catch (Exception e){
+           System.err.println("Error"+e.getMessage());
+       }
+   
+     
+    
+        return mysqlconfig;
+}
+}
